dfe714f42098ed1251a5e342c59e518be1c5b1a4
David Piscasio <82606289+davidpiscasio@users.noreply.github.com>
2022-05-28 13:03:55 +0800

    Changed patch_num

--- train_arguments.py
+++ train_arguments.py
@@ -5,7 +5,7 @@ def get_args():
     parser.add_argument('--depth', type=int, default=12, help='depth')
     parser.add_argument('--embed_dim', type=int, default=64, help='embedding dimension')
     parser.add_argument('--num_heads', type=int, default=8, help='num_heads')
-    parser.add_argument('--patch_num', type=int, default=8, help='patch_num')
+    parser.add_argument('--patch_num', type=int, default=16, help='patch_num')
 
     # where dataset will be stored
     parser.add_argument("--path", type=str, default="data/speech_commands/")
